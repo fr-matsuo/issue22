1b201b7704ea8607527128721635e2039f5ed774
fr-matsuo <matsuo@fact-real.com>
2014-05-02 03:30:04 +0000

    やり残していた課題を終了 #22

--- problem1.php
+++ problem1.php
@@ -1,3 +1,11 @@
 <?php
-printf("1+2=%d\n",1+2);
+//1+2を出力
+$result = 1+2;
+printf("result <- 1 + 2 = %d\n", $result);
+
+//結果から5を引いた結果を出力
+printf("result - 5 = %d\n", $result-5);
+
+//10202/243の余りを出力
+printf("10202 %% 243 = %d\n", 10202%243);
 ?>
